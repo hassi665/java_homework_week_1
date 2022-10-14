d819472ab77fece8e87db58ed7cee36265954de8
hassi665 <hassi665@gmail.com>
2022-10-14 14:50:56 +0100

    java_homework_week_1

---
+++ src/Programme_2_PrintFace.java
@@ -0,0 +1,10 @@
+public class Programme_2_PrintFace {
+
+        public static void main(String[] args) {
+            System.out.println(" +\"\"\"\"\"+");
+            System.out.println("[| o o |]");
+            System.out.println(" |  ^  | ");
+            System.out.println(" | '_' | ");
+            System.out.println(" +_____+");
+        }
+}
